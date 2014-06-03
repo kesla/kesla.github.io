05ebbc0dd901d3037970a8e22df0c2ca75c274d0
David Björklund <david.bjorklund@gmail.com>
2014-06-04 00:37:57 +0200

    gulp: watch mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp')
-  , prefix = require('gulp-autoprefixer');
+  , prefix = require('gulp-autoprefixer')
+  , paths = {
+      css: './source/*.css'
+    };
 
-gulp.task('default', function () {
-  return gulp.src('./source/*.css')
+gulp.task('css', function () {
+  gulp.src(paths.css)
     .pipe(prefix())
     .pipe(gulp.dest('./build'))
 })
+
+gulp.task('watch', function () {
+  gulp.watch(paths.css, [ 'css' ])
+})
+
+gulp.task('default', ['css', 'watch'])
